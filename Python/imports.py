39a653fc03c64e770fef8b7c2af9667e76103b07
Ivan Saji Abraham <ivansajimeleth@gmail.com>
2021-06-09 20:28:20 +0530

    dasdsad

--- Python/imports.py
+++ Python/imports.py
@@ -1,17 +1,12 @@
 import math
-#
-# Complete the 'calc' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts INTEGER c as parameter.
-#
 
 def calc(c):
     # Write your code here
     c=float(c)
     r = float(c/(2*math.pi))
+    
+    area = float(math.pi*(math.pow(r,2)))
     r=round(r,2)
-    area = float(math.pi*(math.pow(r,3)))
     area=round(area,2)
     
     return r,area
