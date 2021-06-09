0485c453e4da4387a42439a81f015a8b9247a250
Ivan Saji Abraham <ivansajimeleth@gmail.com>
2021-06-09 18:14:30 +0530

    dsds

---
+++ Python/namespace.py
@@ -0,0 +1,11 @@
+def Assign(i, f, s, b):
+    # Write your code here
+    w=int(i)
+    x=float(f)
+    y=str(s)
+    if(b=="True"):
+        z=True
+    else:
+        z=False
+    
+    print(w,x,y,z,dir(),sep="\n")
