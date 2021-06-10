9b3a8641b875f117382c25bb477084cbb0fefaef
Ivan Saji Abraham <ivansajimeleth@gmail.com>
2021-06-10 15:11:23 +0530

    dsdsds

---
+++ Python/string Operations 1.py
@@ -0,0 +1,15 @@
+def stringoperation(fn, ln, para, number):
+    # Write your code here
+    req_str=fn
+    for i in range(0,number):
+        req_str=req_str+"\n"
+    req_str=req_str+ln
+    
+    print(req_str
+    )
+    print(fn+"\t"+ln)
+    
+    for i in range(0,number):
+        print(fn,end="")
+    
+    print("\nThe sentence is "+para)
