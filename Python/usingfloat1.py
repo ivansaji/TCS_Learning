cd30f93edd0c9d8e3e9f82aac28d6f4dd13a99a4
Ivan Saji Abraham <ivansajimeleth@gmail.com>
2021-06-10 12:49:52 +0530

    dsdsd

---
+++ Python/usingfloat1.py
@@ -0,0 +1,10 @@
+def triangle(n1, n2, n3):
+    # Write your code here
+    n1=float(n1)
+    n2=float(n2)
+    n3=int(n3)
+    
+    pi=round(math.pi,n3)
+    
+    area=round((n1*n2)/2,n3)
+    return area,pi
