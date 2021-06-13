de695f40b7f31ab8dcd9ebac45bd3419216df459
Ivan Saji Abraham <ivansajimeleth@gmail.com>
2021-06-13 22:04:07 +0530

    dadad

--- Python/StringOperations2.py
+++ Python/StringOperations2.py
@@ -1,27 +1,16 @@
 def resume(first, second, parent, city, phone, start, strfind, string1):
     # Write your code here
-    first=first.replace(" ","").capitalize()
-    second=second.replace(" ","").capitalize()
-    parent=parent.replace(" ","").capitalize()
-    city=city.replace(" ","")
-    
-    new_str=first+" "+second+" "+parent+" "+city
-    print(new_str)
-    
-    if phone.isdigit():
-        print("True")
-    else:
-        print("False")
-    
-    if phone[0]==start:
-        print("True")
-    else:
-        print("False")
-    
-    print(new_str.count(strfind))
-    
-    x=string1.split()
-    print(x)
-    
+    first = first.strip()
+    first = first.capitalize()
+    second = second.strip()
+    second = second.capitalize()
+    parent = parent.strip()
+    parent = parent.capitalize()
+    print(first,second,parent,city.strip())
+    print(phone.isnumeric())
+    print(phone.startswith(start))
+    a = first+second+parent+city
+    print(a.count(strfind))
+    print(string1.split())
     print(city.find(strfind))
     
