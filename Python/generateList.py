ffd44096123cc54d8e8a1b3978a6c0d9b6accad8
Ivan Saji Abraham <ivansajimeleth@gmail.com>
2021-06-14 12:33:52 +0530

    dsds

--- Python/generateList.py
+++ Python/generateList.py
@@ -5,7 +5,7 @@ def generateList(startvalue, endvalue):
         a.append(i)
     print(a)
     b=[]
-    for i in range(endvalue,startvalue+1,-1):
+    for i in range(endvalue,endvalue-5,-1):
         b.append(i)
     print(b)
     c=[]    
@@ -13,6 +13,6 @@ def generateList(startvalue, endvalue):
         c.append(i)
     print(c)
     d=[]
-    for i in range(endvalue,startvalue,-2):
+    for i in range(endvalue,startvalue-1,-2):
         d.append(i)
     print(d)
